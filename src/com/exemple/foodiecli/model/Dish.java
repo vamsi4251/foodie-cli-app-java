d0814a0fa4f4936fda5e2039b1639e51b8b192bf
VAMSI <20551a4251.vamsi@gmail.com>
2024-07-04 19:32:58 +0530

    chore: Add CSV files in data folder

---
+++ src/com/exemple/foodiecli/model/Dish.java
@@ -0,0 +1,59 @@
+package com.exemple.foodiecli.model;
+
+public class Dish {
+
+	
+	 /*
+    add the following properties
+    --------------------------------------
+    Datatype                  variable
+    --------------------------------------
+    String                      id
+    String                      name
+    String                      description
+    double                      price
+     */
+
+    /*
+    1. All the fields should be private
+    2. Create only no-arg constructor
+    3. Create Getters and Setter methods
+    4. Override hashCode() and equals() methods
+    5. Override toString() methods
+     */
+	
+	private String id;
+    private String name;
+    private String description;
+    private double price;
+    
+    
+    // Getter for dishId
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	
+}
---
+++ src/com/exemple/foodiecli/util/CsvReader.java
@@ -0,0 +1,5 @@
+package com.exemple.foodiecli.util;
+
+public class CsvReader {
+
+}
